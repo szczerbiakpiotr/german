0cde5845b94fe96e4e3fd78cf45e168d0d3a29dd
Piotr Szczerbiak <piotr.szczerbiak@accenture.com>
2017-07-05 14:36:09 +0200

    Initial commit

---
+++ src/main/java/com/szczerbiak/words/GermanApplication.java
@@ -0,0 +1,12 @@
+package com.szczerbiak.words;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GermanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GermanApplication.class, args);
+	}
+}
